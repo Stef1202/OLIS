545bc8074765192f7a6da0a37d9941fd6bd57cc2
Stefanie Asaytono <110073396+Stef1202@users.noreply.github.com>
2023-09-28 14:41:12 +0800

    Commit and push

---
+++ online_library_information_system/Developers/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CCT OLIS</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="../design.css">
+
+	<link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	
+	
+	
+	
+	
+	
+	
+	
+  <!-- Bootsrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
+    <link rel="icon" type="image/png" href="../front/images/icons/cctloogo.png"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../front/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../front/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../front/fonts/fontawesome-5.0.8/css/fontawesome-all.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../front/fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../front/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../front/vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../front/vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../front/css/util.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../front/css/main.css">
+    <!-- Javascript -->
+    <script src="../action.js"></script>
+
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.css" integrity="sha512-2dJkRM/DmWkZqINs3QixNKKsgG9mlBT9/PieLVF8OEGHCpPNBoPFYmGPL/yD7JuQVVm2IESF5K0zTDBaf4qehQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+</head>
+
+    <style>
+        blockquote {
+            border-style: solid;
+            border-width: 5px;
+            font-size: 15px;
+            font-style: italic;
+            border-color: #eee #17b978;
+            border-left: 5px solid #17b978;
+            padding:20px;
+        }
+    </style>
+<body>
+
+	<section class="team">
+	    <header>
+    <!-- Header desktop -->
+
+        <div class = "topbar">
+        <div class="top_bar">
+                    <a href="/index.php">Home</a>
+                    <a href="/about.php">About</a>
+                    <a href="/contact.php">Contact Us</a>
+                    <a href="/signin.php">Login</a>
+        </div></div>
+        <!-- Mobile -->
+        <div class="mobile-header-container">
+        
+        <div class="wrap-header-mobile">
+            <!-- Button show menu -->
+            <div class="btn-show-menu-mobile hamburger hamburger--squeeze m-r--8">
+								<span class="hamburger-box">
+									<span class="hamburger-inner"></span>
+								</span>
+            </div>
+        </div></div>
+
+
+</header>
+		<div class="center">
+			<h1>Developers</h1>
+		</div>
+
+		<div class="team-content">
+			<div class="box">
+				<img src="img/a.jpg">
+				<h3>Allan A. Gonzales Jr.</h3>
+				<h5>Programmer<br>Documentation</h5>
+				<div class="icons">
+					<!--<a href=""><i class="ri-twitter-fill"></i></a> -->
+					<a href="https://www.facebook.com/allan.gonzales3"><i class="ri-facebook-box-fill"></i></a>
+					<!--	<a href="#"><i class="ri-instagram-fill"></i></a> -->
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="img/s.jpg">
+				<h3>Stefanie Asaytono</h3>
+				<h5>Programmer<br>Documentation</h5>
+				<div class="icons">
+					<!--	<a href="#"><i class="ri-twitter-fill"></i></a>  -->
+					<a href="https://www.facebook.com/stefanie.asaytono.5"><i class="ri-facebook-box-fill"></i></a>
+					<!--	<a href="#"><i class="ri-instagram-fill"></i></a>  -->
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="img/j.jpg">
+				<h3>Jonjon Salazar Acuña</h3>
+				<h5>Documentation</h5>
+					<h5> </h5>
+				<div class="icons">
+					<!--	<a href="#"><i class="ri-twitter-fill"></i></a> -->
+					<a href="https://www.facebook.com/jonjonacuna.pogi"><i class="ri-facebook-box-fill"></i></a>
+					<!--	<a href="#"><i class="ri-instagram-fill"></i></a>  -->
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="img/e.jpg">
+				<h3>Eugene Lorico</h3>
+				<h5>Documentation</h5>
+					<h5> </h5>
+				<div class="icons">
+	
+					<a href="https://www.facebook.com/profile.php?id=100075980023233"><i class="ri-facebook-box-fill"></i></a>
+	
+				</div>
+			</div>
+
+		</div>
+	</section>
+
+	<?php
+	// Dynamic PHP code can be added here
+	?>
+
+</body>
+</html>
+<?php include_once('hfooter.php');?>
